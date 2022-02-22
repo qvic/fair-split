a57025a5fd8bf927f3cc23f23b36fd9ed4b1157f
Viktor Ovsiichuk <victor.ovseychuk@gmail.com>
2022-02-22 17:01:04 +0200

    Moved parsing from Transfer class

--- src/main/java/org/qvic/model/Transfer.java
+++ src/main/java/org/qvic/model/Transfer.java
@@ -1,51 +1,10 @@
 package org.qvic.model;
 
-import org.qvic.result.Result;
-
 public record Transfer(Account from, Account to, int amount) {
 
     public Transfer {
         if (amount <= 0) {
             throw new IllegalArgumentException("Transfer amount should be positive");
-        }
-    }
-
-    public static Result<Transfer> fromLine(String line) {
-        if (line == null) {
-            return Result.err("Line is null");
-        }
-        var split = line.split(",", 3);
-        if (split.length < 3) {
-            return Result.err("Malformed line");
-        }
-
-        var from = parseAccountName(split[0], "From");
-        var to = parseAccountName(split[1], "To");
-        var amount = parseAmount(split[2]);
-
-        return Result.coalesce3(from, to, amount,
-                (f, t, a) -> new Transfer(new Account(f), new Account(t), a),
-                errors -> String.join("; ", errors));
-    }
-
-    private static Result<Integer> parseAmount(String str) {
-        try {
-            var amount = Integer.parseInt(str.trim());
-            if (amount <= 0) {
-                return Result.err("Transfer amount should be positive");
-            }
-            return Result.ok(amount);
-        } catch (NumberFormatException e) {
-            return Result.err("Could not parse transfer amount");
-        }
-    }
-
-    private static Result<String> parseAccountName(String str, String which) {
-        var trim = str.trim();
-        if (trim.isEmpty()) {
-            return Result.err(which + " account name should not be empty");
-        } else {
-            return Result.ok(trim);
         }
     }
 
--- src/main/java/org/qvic/util/IOUtils.java
+++ src/main/java/org/qvic/util/IOUtils.java
@@ -1,5 +1,6 @@
 package org.qvic.util;
 
+import org.qvic.model.Account;
 import org.qvic.model.Transfer;
 import org.qvic.result.Result;
 
@@ -14,11 +15,50 @@ public class IOUtils {
         return Files.readAllLines(Path.of(filename)).stream()
                 .filter(line -> !line.isEmpty())
                 .<Transfer>mapMulti((line, consumer) -> {
-                    var result = Transfer.fromLine(line);
+                    var result = parseTransfer(line);
                     result.consume(consumer,
                             err -> System.out.printf("[WARN] Could not parse line '%s': %s\n", line, err));
                 })
                 .toList();
+    }
+
+    public static Result<Transfer> parseTransfer(String line) {
+        if (line == null) {
+            return Result.err("Line is null");
+        }
+        var split = line.split(",", 3);
+        if (split.length < 3) {
+            return Result.err("Malformed line");
+        }
+
+        var from = parseTransferAccountName(split[0], "From");
+        var to = parseTransferAccountName(split[1], "To");
+        var amount = parseTransferAmount(split[2]);
+
+        return Result.coalesce3(from, to, amount,
+                (f, t, a) -> new Transfer(new Account(f), new Account(t), a),
+                errors -> String.join("; ", errors));
+    }
+
+    private static Result<Integer> parseTransferAmount(String str) {
+        try {
+            var amount = Integer.parseInt(str.trim());
+            if (amount <= 0) {
+                return Result.err("Transfer amount should be positive");
+            }
+            return Result.ok(amount);
+        } catch (NumberFormatException e) {
+            return Result.err("Could not parse transfer amount");
+        }
+    }
+
+    private static Result<String> parseTransferAccountName(String str, String which) {
+        var trim = str.trim();
+        if (trim.isEmpty()) {
+            return Result.err(which + " account name should not be empty");
+        } else {
+            return Result.ok(trim);
+        }
     }
 
     public static void writeToFile(String filename, List<Transfer> transfers) throws IOException {
